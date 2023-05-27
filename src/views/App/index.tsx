eec778e882ac512898f698b120cd55877daafee3
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-05-27 16:56:14 +0300

    feat: add base App.ts

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import styles from "./index.module.scss";
+
 export const App: React.FC = () => {
-  return <div>index</div>;
+  return (
+    <section className={styles.todo}>
+      <h1 className={styles.todo__title}>TODO App</h1>
+      <div className={styles.todo__block}></div>
+      <div className={styles.todo__block}></div>
+    </section>
+  );
 };
